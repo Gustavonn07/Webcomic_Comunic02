53d38d26d35d10e0ab56681c14d85645bdad61ba
Gustavonn07 <gustavonepnog@gmail.com>
2026-01-21 09:25:35 -0300

    refact: change name pannels images

--- src/app/chapter/1/pannels.ts
+++ src/app/chapter/1/pannels.ts
@@ -1,30 +1,30 @@
 export const panels = [
   {
-    src: "/chapter-1/Prancheta 1.png",
+    src: "/chapter-1/Prancheta-1.png",
     alt: "Painel 1",
     width: 720,
     height: 405,
   },
   {
-    src: "/chapter-1/Prancheta 2.png",
+    src: "/chapter-1/Prancheta-2.png",
     alt: "Painel 2",
     width: 720,
     height: 405,
   },
   {
-    src: "/chapter-1/Prancheta 3.png",
+    src: "/chapter-1/Prancheta-3.png",
     alt: "Painel 3",
     width: 720,
     height: 405,
   },
   {
-    src: "/chapter-1/Prancheta 4.png",
+    src: "/chapter-1/Prancheta-4.png",
     alt: "Painel 4",
     width: 720,
     height: 405,
   },
   {
-    src: "/chapter-1/Prancheta 5.png",
+    src: "/chapter-1/Prancheta-5.png",
     alt: "Painel 5",
     width: 720,
     height: 405,
