18541e47a2080cae1b316f792075a7327b43ea14
Gustavonn07 <gustavonepnog@gmail.com>
2026-01-22 08:16:15 -0300

    fix: baloons responsive

--- src/app/chapter/2/page.tsx
+++ src/app/chapter/2/page.tsx
@@ -10,18 +10,82 @@ export default function Chapter2Page() {
   const [loaded, setLoaded] = useState<boolean>(false)
 
   return (
-    <div className="relative select-none w-[180vh] h-[100vw] lg:w-11/12 lg:h-full">
+    <div className="relative select-none w-[180vh] h-[100vw] md:w-11/12 md:h-full">
       <ComicImage
         src="/chapter-2/Prancheta-6.png"
         alt="Painel 6"
         width={720}
         height={405}
-        className={cn('w-[180vh] lg:w-full lg:h-full h-[300vw]')}
+        className={cn('w-[180vh] md:w-full md:h-full h-[300vw]')}
         onLoaded={() => setLoaded(true)}
       />
-      { loaded && (<><Balloon variant={VariantTypes.WAVE} className='absolute bottom-[45vh] left-[48vw] scale-100 hover:scale-105 3xl:scale-125 hover:3xl:scale-150' />
-      <Balloon variant={VariantTypes.WALK} className='absolute bottom-[45vh] left-[32.5vw] scale-100 hover:scale-105 3xl:scale-125 hover:3xl:scale-150' />
-      <Balloon variant={VariantTypes.SCREAM} className='absolute bottom-[50vh] left-[60vw] scale-100 hover:scale-105 3xl:scale-125 hover:3xl:scale-150' /></>)}
+      {loaded && (
+      <>
+        <Balloon
+            variant={VariantTypes.WAVE}
+            className="
+              absolute
+
+              portrait:left-[46%]
+              portrait:bottom-[13%]
+
+              landscape:left-[46%]
+              sm:landscape:bottom-[-169%]
+              md:landscape:bottom-[7%]
+              lg:landscape:bottom-[11%]
+
+              scale-80
+              md:scale-60
+              lg:scale-100
+              hover:scale-105
+              3xl:scale-120
+            "
+          />
+
+          <Balloon
+            variant={VariantTypes.WALK}
+            className="
+              absolute
+
+              portrait:left-[32%]
+              portrait:bottom-[10%]
+
+              landscape:left-[60%]
+              sm:landscape:bottom-[-170%]
+              md:landscape:bottom-[8%]
+              lg:landscape:bottom-[10.5%]
+
+              scale-80
+              md:scale-60
+              lg:scale-100
+              hover:scale-105
+              3xl:scale-120
+            "
+          />
+
+          <Balloon
+            variant={VariantTypes.SCREAM}
+            className="
+              absolute
+
+              portrait:left-[60%]
+              portrait:bottom-[9%]
+
+              landscape:left-[31%]
+              sm:landscape:bottom-[-170.5%]
+              md:landscape:bottom-[8.5%]
+              lg:landscape:bottom-[11%]
+
+              scale-80
+              sm:scale-70
+              md:scale-60
+              lg:scale-100
+              hover:scale-105
+              3xl:scale-120
+            "
+          />
+      </>
+    )}
     </div>
   )
 }
